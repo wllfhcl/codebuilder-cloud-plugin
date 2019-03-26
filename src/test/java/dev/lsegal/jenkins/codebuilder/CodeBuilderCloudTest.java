76d9c9f4960a4ca7fad9d9ff3140305385219753
Loren Segal <lsegal@soen.ca>
2019-03-26 12:54:34 -0700

    Add some basic tests (more to come)

--- src/test/java/dev/lsegal/jenkins/codebuilder/CodeBuilderCloudTest.java
+++ src/test/java/dev/lsegal/jenkins/codebuilder/CodeBuilderCloudTest.java
@@ -1,4 +1,21 @@
 package dev.lsegal.jenkins.codebuilder;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
 public class CodeBuilderCloudTest {
+  @Rule
+  public JenkinsRule j = new JenkinsRule();
+
+  @Test
+  public void initializes_correctly() throws InterruptedException {
+    CodeBuilderCloud cloud = new CodeBuilderCloud(null, "project", null, null);
+    assertEquals("project", cloud.getProjectName());
+    assertEquals("codebuilder_0", cloud.getDisplayName());
+    assertNotNull(cloud.getClient());
+  }
 }
