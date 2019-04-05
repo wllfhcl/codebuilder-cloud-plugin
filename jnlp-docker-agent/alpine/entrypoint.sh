1d18fb8f5bf5d8c4b84f4cc68bec9cbae028cafc
Loren Segal <lsegal@soen.ca>
2019-04-05 14:04:39 -0700

    Use connect-timeout instead of full max time

--- jnlp-docker-agent/alpine/entrypoint.sh
+++ jnlp-docker-agent/alpine/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 AZURL="http://169.254.169.254/latest/meta-data/placement/availability-zone"
-export AWS_DEFAULT_REGION=`curl -m 0.5 -s $AZURL | sed 's/[a-z]$//'`
+export AWS_DEFAULT_REGION=`curl --connect-timeout 0.5 -s $AZURL | sed 's/[a-z]$//'`
 
 exec /usr/local/bin/jenkins-agent "$@"
