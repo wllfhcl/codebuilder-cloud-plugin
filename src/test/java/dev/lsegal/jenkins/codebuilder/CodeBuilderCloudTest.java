60f8ab253d3757b682320505642b669d58c2a4e6
Loren Segal <lsegal@soen.ca>
2020-05-01 19:56:30 -0700

    Fix test failure

--- src/test/java/dev/lsegal/jenkins/codebuilder/CodeBuilderCloudTest.java
+++ src/test/java/dev/lsegal/jenkins/codebuilder/CodeBuilderCloudTest.java
@@ -13,7 +13,7 @@ public class CodeBuilderCloudTest {
 
   @Test
   public void initializes_correctly() throws InterruptedException {
-    CodeBuilderCloud cloud = new CodeBuilderCloud(null, "project", null, null);
+    CodeBuilderCloud cloud = new CodeBuilderCloud(null, "project", null, "us-west-2");
     assertEquals("project", cloud.getProjectName());
     assertEquals("codebuilder_0", cloud.getDisplayName());
     assertNotNull(cloud.getClient());
